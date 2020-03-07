77eac7b4857ea372f5250b8f4b68d979bf01b20c
John Kiouris <kiouris@Johns-MacBook-Pro.local>
2020-03-07 12:20:57 -0500

    Initial commit

---
+++ src/main/java/com/temple/polymorphic/toolbox/PolymorphicToolboxApplication.java
@@ -0,0 +1,13 @@
+package com.temple.polymorphic.toolbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolymorphicToolboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolymorphicToolboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/temple/polymorphic/toolbox/PolymorphicToolboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temple.polymorphic.toolbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolymorphicToolboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
