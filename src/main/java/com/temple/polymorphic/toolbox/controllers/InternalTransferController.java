c99586806ae852a5570ef387e216642408cb1950
Bella Yang <hwy4012@gmail.com>
2020-03-27 16:48:56 -0400

    commiting UI

---
+++ src/main/java/com/temple/polymorphic/toolbox/controllers/InternalTransferController.java
@@ -0,0 +1,10 @@
+package com.temple.polymorphic.toolbox.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/restore")
+public class InternalTransferController {
+}
