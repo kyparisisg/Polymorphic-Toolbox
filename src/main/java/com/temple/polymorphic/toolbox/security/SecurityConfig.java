5a920738bf8fa3605102055a43839e47f59dba28
Giannis Kiouris Kyparisis (Alias John Kiouris) <tuk12920@temple.edu>
2020-03-28 20:41:32 -0400

    Security Auth

--- src/main/java/com/temple/polymorphic/toolbox/security/SecurityConfig.java
+++ src/main/java/com/temple/polymorphic/toolbox/security/SecurityConfig.java
@@ -1,5 +1,18 @@
 package com.temple.polymorphic.toolbox.security;
 
-public class SecurityConfig {
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 
+public class SecurityConfig implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
 }
