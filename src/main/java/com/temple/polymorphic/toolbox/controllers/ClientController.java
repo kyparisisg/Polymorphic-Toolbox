d5aeaafad2b684222cc8f6574fd28a6cceb217cb
Giannis Kiouris Kyparisis (Alias John Kiouris) <tuk12920@temple.edu>
2020-04-07 19:29:40 -0400

    crafting client controller

---
+++ src/main/java/com/temple/polymorphic/toolbox/controllers/ClientController.java
@@ -0,0 +1,24 @@
+package com.temple.polymorphic.toolbox.controllers;
+
+
+import com.temple.polymorphic.toolbox.services.TransferService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/client")
+public class ClientController {
+
+    @Autowired
+    private TransferService serverService;
+
+    @RequestMapping(value = "/transfer" , method = RequestMethod.GET)
+    public ModelAndView getViewForTransfer(){
+
+        return new ModelAndView("/client/")
+    }
+}
