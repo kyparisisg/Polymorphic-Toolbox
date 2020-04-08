abe88804a50410836b0e890da0c67b998b17dd2c
Tyler Allen <tyyen5@gmail.com>
2020-04-08 14:15:37 -0400

    some basic functionality for creating a new bucket for S3

---
+++ src/main/java/com/temple/polymorphic/toolbox/services/BucketTools.java
@@ -0,0 +1,64 @@
+package com.temple.polymorphic.toolbox.services;
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+public class BucketTools {
+    private static final boolean IS_WINDOWS = System.getProperty( "os.name" ).contains( "indow" );
+
+    public static void deleteBucket(String bucketName,AmazonS3Client s3client) throws IOException {
+        try {
+            System.out.println("Deleting S3 bucket: " + bucketName);
+            ObjectListing objectListing = s3client.listObjects(bucketName);
+
+            while (true) {
+                for ( Iterator<?> iterator = objectListing.getObjectSummaries().iterator(); iterator.hasNext(); ) {
+                    S3ObjectSummary objectSummary = (S3ObjectSummary) iterator.next();
+                    s3client.deleteObject(bucketName, objectSummary.getKey());
+                }
+                if (objectListing.isTruncated()) {
+                    objectListing = s3client.listNextBatchOfObjects(objectListing);
+                } else {
+                    break;
+                }
+            };
+            VersionListing list = s3client.listVersions(new ListVersionsRequest().withBucketName(bucketName));
+            for ( Iterator<?> iterator = list.getVersionSummaries().iterator(); iterator.hasNext(); ) {
+                S3VersionSummary s = (S3VersionSummary)iterator.next();
+                s3client.deleteVersion(bucketName, s.getKey(), s.getVersionId());
+            }
+            s3client.deleteBucket(bucketName);
+
+
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which " +
+                    "means your request made it " +
+                    "to Amazon S3, but was rejected with an error response" +
+                    " for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which " +
+                    "means the client encountered " +
+                    "an internal error while trying to " +
+                    "communicate with S3, " +
+                    "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+    }
+
+
+
+}
--- src/main/java/com/temple/polymorphic/toolbox/services/TransferService.java
+++ src/main/java/com/temple/polymorphic/toolbox/services/TransferService.java
@@ -9,8 +9,45 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
 @Service
 public class TransferService {
+
+    public static void CnPSimpleBucket(){
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(Credentials.access_key_id, Credentials.secret_access_key);
+
+        AmazonS3Client s3Client = new AmazonS3Client(awsCreds);
+
+        // BucketUtils.deleteAllBuckets(s3Client);
+
+
+        String newBucketName = "matt" + System.currentTimeMillis();
+
+
+        s3Client.createBucket(newBucketName);
+
+        final String fileName = "sometext.txt";
+
+//        File file = new File(S3JavaSDKExample.class.getResource(fileName).toURI());
+
+        //        this is anew bucket and folders dont exist in S3, S3 is just a key value store.
+//        {
+//
+//        }
+
+    }
 
     //implement the services here!
 
