a5b432bafcdc5c149ee2ba2d6d7fd9a0724dcc59
Giannis Kiouris Kyparisis (Alias John Kiouris) <tuk12920@temple.edu>
2020-04-14 23:18:02 -0400

    Refactoring authentication and encryption

--- src/main/java/com/temple/polymorphic/toolbox/services/MySimpleUrlAuthenticationSuccessHandler.java
+++ src/main/java/com/temple/polymorphic/toolbox/services/MySimpleUrlAuthenticationSuccessHandler.java
@@ -1,13 +1,8 @@
 package com.temple.polymorphic.toolbox.services;
 
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
