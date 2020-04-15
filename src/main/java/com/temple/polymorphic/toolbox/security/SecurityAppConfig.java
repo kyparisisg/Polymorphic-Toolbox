de9384e8beaacf7d2c91d3c0dbe90c658a0d5c2f
Giannis Kiouris Kyparisis (Alias John Kiouris) <tuk12920@temple.edu>
2020-04-14 23:28:26 -0400

    Refactoring random token generation method

--- src/main/java/com/temple/polymorphic/toolbox/security/SecurityAppConfig.java
+++ src/main/java/com/temple/polymorphic/toolbox/security/SecurityAppConfig.java
@@ -9,15 +9,12 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-import javax.servlet.http.Cookie;
 import javax.sql.DataSource;
 
 @Configuration
