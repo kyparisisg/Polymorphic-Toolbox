d24c7bf1d04dbd235bc4abac4e2827eaefed2b89
Giannis Kiouris Kyparisis (Alias John Kiouris) <tuk12920@temple.edu>
2020-04-22 21:18:30 -0400

    Bucket name bug is fixed

--- src/main/java/com/temple/polymorphic/toolbox/services/Credentials.java
+++ src/main/java/com/temple/polymorphic/toolbox/services/Credentials.java
@@ -1,7 +1,7 @@
 package com.temple.polymorphic.toolbox.services;
 
 public class Credentials {
-    public static final String bucketNameC = "polymorphic";
+    public static final String bucketNameC = "greekarmy";
     public static final String access_key_id= "AKIAJBU3EAP7P6YEEQIQ";
     public static final String secret_access_key ="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";
 
